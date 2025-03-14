954be339da1774bd8c86d09075e9615eaa234d5b
24LP3201 24LP3201 <24LP3201@G304-CC0219.local>
2025-03-14 12:49:41 +0800

    edit

--- src/app/(admin)/dishes/page.tsx
+++ src/app/(admin)/dishes/page.tsx
@@ -1,142 +1,3 @@
-// "use client";
-// import Image from "next/image";
-// import { NomNom } from "../toggle-menu/page";
-// import { Button } from "@/components/ui/button";
-// import {
-//   Dialog,
-//   DialogContent,
-//   DialogFooter,
-//   DialogHeader,
-//   DialogTitle,
-//   DialogTrigger,
-// } from "@/components/ui/dialog";
-// import { Input } from "@/components/ui/input";
-// import { Label } from "@/components/ui/label";
-// import { useEffect, useState } from "react";
-
-// // import { CloudnaryUpload } from "@/app/_components/Cloudnary";
-
-// export default function Dishes() {
-//   const [categories, setCategories] = useState([]);
-//   const [categoryName, setCategoryName] = useState("");
-
-//   useEffect(() => {
-//     const getCategories = async () => {
-//       const data = await fetch("http://localhost:7000/food-category");
-//       const jsondData = await data.json();
-//       // setCategories(jsondData.data);
-//       console.log("back-end data", jsondData);
-//       setCategories(jsondData.newGetCategory);
-//     };
-//     getCategories();
-//   }, []);
-
-//   const createCategory = async () => {
-//     const data = await fetch("http://localhost:7000/food-category", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ categoryName }),
-//     });
-//   };
-
-//   const handleChange = (e: any) => {
-//     const { value } = e.target;
-//     setCategoryName(value);
-//   };
-
-//   return (
-//     <div className="flex w-[1376px]">
-//       <div className="w-[205px]">
-//         <NomNom />
-//       </div>
-//       <div className="w-[1171px] h-[582px] ">
-//         <div className="flex gap-5 justify-center">
-//           {categories?.map((category: any, index: any) => {
-//             return <div key={index}>{category.categoryName}</div>;
-//           })}
-//         </div>
-//         <div className="w-[28px] h-[28px]">
-//           <h2 className="text-[20px]"></h2>
-//         </div>
-//         <div className="w-[1131px] h-[241px] items-center edrounded-lg flex gap-3">
-//           <div className="relative border-[1px]">
-//             <Image
-//               src="/"
-//               alt=""
-//               width={271}
-//               height={241}
-//               className="w-[241px] h-[200px]"
-//             />
-//             <div className="flex flex-col items-center">
-//               <p>Add new Dish to </p>
-//               <p>Salads</p>
-//             </div>
-//             <Dialog>
-//               <DialogTrigger asChild>
-//                 <Button
-//                   variant="outline"
-//                   className="absolute bottom-[50%] left-[40%] rounded-full bg-[#EF4444] w-[40px] h-[40px]"
-//                 >
-//                   +
-//                 </Button>
-//               </DialogTrigger>
-//               <DialogContent className="sm:max-w-[425px]">
-//                 <DialogHeader>
-//                   <DialogTitle>Add new Dish to Appetizers</DialogTitle>
-//                 </DialogHeader>
-//                 <div className="grid gap-4 py-4">
-//                   <div className="grid grid-cols-4 items-center gap-4">
-//                     <Label htmlFor="name" className="text-right">
-//                       Food name
-//                     </Label>
-//                     <Input
-//                       id="name"
-//                       onChange={handleChange}
-//                       value={categoryName}
-//                       className="col-span-3"
-//                     />
-//                   </div>
-//                   {/* <div className="grid grid-cols-4 items-center gap-4">
-//                     <Label htmlFor="username" className="text-right">
-//                       Food price
-//                     </Label>
-//                     <Input
-//                       id="username"
-//                       // value="placeholder"
-//                       className="col-span-3"
-//                     />
-//                   </div> */}
-//                   {/* <div>
-//                     <CloudnaryUpload />
-//                   </div> */}
-//                 </div>
-//                 <DialogFooter>
-//                   <Button onClick={createCategory}>Add dishes</Button>
-//                 </DialogFooter>
-//               </DialogContent>
-//             </Dialog>
-//           </div>
-//           <div className="flex object-contain flex-col w-[241px] items-center">
-//             <div className="flex justify-center p-1">
-//               <Image width={270} height={241} src="/Product Image.png" alt="" />
-//             </div>
-//             <div className="flex gap-2 w-[238.75px] h-[20px]">
-//               <h2 className="text-[#EF4444]">Brie Crostini Appetizer</h2>
-//               <p>12.99$</p>
-//             </div>
-//             <p>
-//               Fluffy pancakes stacked with fruits, cream, syrup, and powdered
-//               sugar.
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -163,13 +24,16 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
+  DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Category } from "@/app/types";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { CloudnaryUpload } from "@/app/_components/Cloudnary";
+import { Label } from "@/components/ui/label";
+import CloudnaryUploadd from "@/app/_components/Cloudnary";
 
 const formSchema = z.object({
   categoryName: z.string().min(2, "CategoryName must be at least 2 characters"),
@@ -178,8 +42,8 @@ const formSchema = z.object({
 export default function ProfileFormDishes() {
   const [categories, setCategories] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
-  const [isEdit, setIsEdit] = useState(false);
-  const [saveId, setSaveId] = useState<string>("");
+  const [isEdit, setIsEdit] = useState("");
+  const [categoryId, setCategoryId] = useState("");
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -213,8 +77,16 @@ export default function ProfileFormDishes() {
   const updateCategory = async (id: string) => {
     const data = await fetch(`http://localhost:7000/food-category/${id}`, {
       method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ categoryName: isEdit }),
     });
-    getCategories;
+    getCategories();
+  };
+  const handleclick = (e: any) => {
+    const { value } = e.target;
+    setIsEdit(value);
   };
 
   const deleteCategory = async (id: string) => {
@@ -225,16 +97,10 @@ export default function ProfileFormDishes() {
     getCategories();
   };
 
-  const editHandleClick = (id: string) => {
-    setIsEdit(true);
-    // setSaveId(true);
-  };
-
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
     createCategory(values.categoryName);
     setIsOpen(false);
-    // updateCategory(values.categoryName);
   }
 
   return (
@@ -244,28 +110,62 @@ export default function ProfileFormDishes() {
           <h1 className="text-[20px]">Dishes category</h1>
         </div>
         <div className="flex gap-2 w-full h-[176px] text-nowrap items-center">
-          {categories?.map((category: Category, index: number) => {
-            return (
-              <ContextMenu key={index}>
-                <ContextMenuTrigger>
-                  <button className="rounded-3xl border-[1px] py-2 px-4 hover:border-blue-600">
-                    {category.categoryName}
-                  </button>
-                </ContextMenuTrigger>
-                <ContextMenuContent>
-                  <ContextMenuItem onClick={() => updateCategory(category._id)}>
-                    Edit
-                  </ContextMenuItem>
-                  <ContextMenuItem onClick={() => deleteCategory(category._id)}>
-                    Delete
-                  </ContextMenuItem>
-                  {/* <ContextMenuItem>Team</ContextMenuItem>
-                      <ContextMenuItem>Subscription</ContextMenuItem> */}
-                </ContextMenuContent>
-              </ContextMenu>
-            );
-          })}
+          <Dialog>
+            {categories?.map((category: Category, index: number) => {
+              return (
+                <ContextMenu key={index}>
+                  <ContextMenuTrigger>
+                    <button className="rounded-3xl border-[1px] py-2 px-4 hover:border-blue-600">
+                      {category.categoryName}
+                    </button>
+                  </ContextMenuTrigger>
+                  <ContextMenuContent>
+                    <DialogTrigger asChild>
+                      <ContextMenuItem
+                        onClick={() => setCategoryId(category._id)}
+                      >
+                        Edit Category
+                      </ContextMenuItem>
+                    </DialogTrigger>
 
+                    <ContextMenuItem
+                      onClick={() => deleteCategory(category._id)}
+                    >
+                      Delete
+                    </ContextMenuItem>
+                  </ContextMenuContent>
+                </ContextMenu>
+              );
+            })}
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle hidden></DialogTitle>
+                <DialogDescription>Edit food category</DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name" className="text-right">
+                    Edit here
+                  </Label>
+                  <Input
+                    onChange={handleclick}
+                    id="name"
+                    className="col-span-3"
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button
+                  type="submit"
+                  onClick={() => {
+                    updateCategory(categoryId);
+                  }}
+                >
+                  Save edit changes
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
           <Dialog open={isOpen} onOpenChange={setIsOpen}>
             <DialogTrigger asChild>
               <button className="w-[36px] h-[36px] bg-[#EF4444] rounded-full text-white gap-8 px-[16px] items-center justify-center flex py-[8px]">
@@ -306,7 +206,7 @@ export default function ProfileFormDishes() {
           <div className="w-[239px] h-[225px] border-[1px] rounded-2xl items-center justify-center flex flex-col gap-2">
             <Button
               onClick={() => {
-                CloudnaryUpload;
+                CloudnaryUploadd;
               }}
               className="text-white bg-[#EF4444] w-[40px] h-[40px] rounded-full items-center flex"
             >
--- src/app/_components/Cloudnary.tsx
+++ src/app/_components/Cloudnary.tsx
@@ -1,66 +1,211 @@
+// "use client";
+
+// import { useState } from "react";
+// import React from "react";
+
+// export const CloudnaryUpload = () => {
+//   const [file, setFile] = useState(null);
+//   const [image, setImage] = useState("");
+
+//   const PRESET_NAME = "food-delivery-app";
+//   const CLOUDINAY_NAME = "deossi8am";
+
+//   const handleFile = (e: any) => {
+//     const file = e.target.files[0];
+//     if (file) {
+//       setFile(file);
+//       setImage(URL.createObjectURL(file));
+//     }
+//   };
+
+//   const handleUpload = async () => {
+//     console.log("image uploading");
+//     if (!file) {
+//       alert("Pls select a file");
+//       return;
+//     }
+//     const formData = new FormData();
+//     formData.append("file", file);
+//     formData.append("upload_preset", PRESET_NAME);
+//     formData.append("api_key", CLOUDINAY_NAME);
+
+//     try {
+//       const res = await fetch(
+//         `http://api.cloudinary.com/v1_1/${CLOUDINAY_NAME}/upload`,
+//         {
+//           method: "POST",
+//           body: formData,
+//         }
+//       );
+//       const data = await res.json();
+//       setImage(data.secure_url);
+//       console.log("image uploaded", data);
+//     } catch (err) {
+//       console.log(err);
+//       alert("Failed to upload file");
+//       setFile(null);
+//     }
+//     console.log(file);
+//   };
+
+//   return (
+//     <div className="">
+//       <div className="flex justify-center items-center w-full">
+//         <input type="file" onChange={handleFile} />
+//         <button onClick={handleUpload} className="bg-green-500 rounded-lg p-1">
+//           Upload
+//         </button>
+//       </div>
+
+//       {image && (
+//         <img alt="uploaded" src={image} className="w-[300px] h-[300px]" />
+//       )}
+//     </div>
+//   );
+// };
+
 "use client";
 
-// import Image from "next/image";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import React from "react";
 
-export const CloudnaryUpload = () => {
-  const [file, setFile] = useState(null);
-  const [image, setImage] = useState("");
+const UPLOAD_PRESET = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!;
+const CLOUD_NAME = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
 
-  const PRESET_NAME = "food-delivery-app";
-  const CLOUDINAY_NAME = "deossi8am";
+const uploadImage = async (file: File | null) => {
+  if (!file) {
+    return null;
+  }
 
-  const handleFile = (e: any) => {
-    const file = e.target.files[0];
-    if (file) {
-      setFile(file);
-      setImage(URL.createObjectURL(file));
-    }
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", UPLOAD_PRESET);
+
+  try {
+    const response = await fetch(
+      `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    const result = await response.json();
+
+    return result.secure_url;
+  } catch (error: unknown) {
+    return { error: "failed to upload image" };
+  }
+};
+
+const formSchema = z.object({
+  foodName: z.string().min(2, "Hamgiin bagadaa 2 usegtei baina shuu"),
+  image: z.string().nonempty("Zuragaa oruulna uu"),
+});
+
+export default function CloudnaryUploadd() {
+  const [foodImageFile, setFoodImageFile] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string>();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      foodName: "",
+      image: "",
+    },
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files![0];
+
+    setFoodImageFile(file);
+
+    const tempImageUrl = URL.createObjectURL(file);
+    setPreviewUrl(tempImageUrl);
+    form.setValue("image", "uploaded");
   };
 
-  const handleUpload = async () => {
-    console.log("image uploading");
-    if (!file) {
-      alert("Pls select a file");
-      return;
-    }
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("upload_preset", PRESET_NAME);
-    formData.append("api_key", CLOUDINAY_NAME);
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    createFood(values);
+  }
 
-    try {
-      const res = await fetch(
-        `http://api.cloudinary.com/v1_1/${CLOUDINAY_NAME}/upload`,
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
-      const data = await res.json();
-      setImage(data.secure_url);
-      console.log("image uploaded", data);
-    } catch (err) {
-      console.log(err);
-      alert("Failed to upload file");
-      setFile(null);
-    }
-    console.log(file);
+  const createFood = async (values: z.infer<typeof formSchema>) => {
+    const imageUrl = await uploadImage(foodImageFile);
+
+    const data = await fetch("http://localhost:8000/food", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        foodName: values.foodName,
+        price: 100,
+        image: imageUrl,
+        ingredients: "guril mah",
+        category: "676e370164d1f8cafda026ac",
+      }),
+    });
+    const jsonData = await data.json();
+
+    console.log("data", jsonData);
   };
 
   return (
-    <div className="">
-      <div className="flex justify-center items-center w-full">
-        <input type="file" onChange={handleFile} />
-        <button onClick={handleUpload} className="bg-green-500 rounded-lg p-1">
-          Upload
-        </button>
-      </div>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="foodName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Food name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Soup" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-      {image && (
-        <img alt="uploaded" src={image} className="w-[300px] h-[300px]" />
+          <FormField
+            control={form.control}
+            name="image"
+            render={({ field: { onChange, value, ...rest } }) => (
+              <FormItem>
+                <FormLabel>Image</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="image"
+                    type="file"
+                    onChange={handleChange}
+                    {...rest}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+
+      {previewUrl && (
+        <div className="border">
+          <img className="size-48 object-cover" src={previewUrl} alt="" />
+        </div>
       )}
     </div>
   );
-};
+}
